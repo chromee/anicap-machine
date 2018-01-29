2d30f84993d9031108610d3586c9d3103db5c12b
chromee <odk.mmmm@gmail.com>
2018-01-29 17:51:25 +0900

    mainにcam_m処理追加

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,10 @@
 from capraping import CapScraper
+from cap_manager import CapManager
 
+cap_dir = r"D:\cap2"
 
-scraper = CapScraper(200, 200, 0, 320, "D:\cap2")
+scraper = CapScraper(200, 200, 0, 1, cap_dir)
 scraper.execute()
 
+# cap_m = CapManager(cap_dir)
+# cap_m.extract_face_and_save()
