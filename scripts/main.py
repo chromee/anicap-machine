9b56992775c432e19d9cd14a8c1e7d2e0d10dc67
chromee <odk.mmmm@gmail.com>
2018-01-30 21:36:02 +0900

    endページ修正

--- scripts/main.py
+++ scripts/main.py
@@ -9,8 +9,8 @@ face_dir = r"D:\cap2\_face"
 # cap_dir = r"/Users/denx_mac/Projects/anicap-machine/data/cap"
 # face_dir = r"/Users/denx_mac/Projects/anicap-machine/data/face"
 
-# scraper = CapScraper(0, 1, cap_dir)
-# scraper.execute()
+scraper = CapScraper(0, 320, cap_dir)
+scraper.execute()
 
 cap_m = CapManager(cap_dir, face_dir)
 cap_m.extract_face_and_save()
