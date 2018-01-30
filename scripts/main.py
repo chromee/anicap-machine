35ce2efa206969ceb5e85da4c5932645ded9c0e0
chromee <odk.mmmm@gmail.com>
2018-01-30 21:33:35 +0900

    windows用のディレクトリを修正

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,16 @@
 from capraping import CapScraper
 from cap_manager import CapManager
 
-## Windows用
+# Windows用
 cap_dir = r"D:\cap2"
-face_dir = r"D:\cap2¥_face"
+face_dir = r"D:\cap2\_face"
 
 # Mac用
 # cap_dir = r"/Users/denx_mac/Projects/anicap-machine/data/cap"
 # face_dir = r"/Users/denx_mac/Projects/anicap-machine/data/face"
 
-scraper = CapScraper(0, 1, cap_dir)
-scraper.execute()
+# scraper = CapScraper(0, 1, cap_dir)
+# scraper.execute()
 
 cap_m = CapManager(cap_dir, face_dir)
 cap_m.extract_face_and_save()
